6479b413419116b21fee123209b82aa175b5042e
Joy0820 <309742645@qq.com>
2018-09-07 14:58:00 +0800

    add grid layout

--- app/src/main/java/com/fxc/myvideoplayer/VideoList/FileListActivity.java
+++ app/src/main/java/com/fxc/myvideoplayer/VideoList/FileListActivity.java
@@ -6,16 +6,24 @@ import android.os.Bundle;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 
+import android.support.v7.widget.StaggeredGridLayoutManager;
 import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.Toast;
 
 
+import com.fxc.myvideoplayer.FolderList.FolderItemDecoration;
 import com.fxc.myvideoplayer.R;
 
 import java.util.List;
 
 public class FileListActivity extends AppCompatActivity {
-ActionBar actionBar;
-//ImageView iv;
+    ActionBar actionBar;
+    RecyclerView videoRecyclerView;
+    RecyclerView.LayoutManager layoutManager;
+    VideoAdapter adapter;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -24,27 +32,44 @@ ActionBar actionBar;
         actionBar = getSupportActionBar();
         String folderName = getIntent().getStringExtra("folder_cat_name");
         actionBar.setTitle(folderName);
-        Log.i("Main","21");
-        //iv = findViewById(R.id.video_image);
-        //iv.setImageResource(R.drawable.item1);
-
 
         List<VideoItems> videos = VideoFactory.create_videos(20);
-        Log.i("name", "onCreate: "+videos.get(0).get_video_name());
-        Log.i("duration", "onCreate: "+videos.get(0).get_video_duration());
-        VideoAdapter adapter = new VideoAdapter(this,videos);
-
-        RecyclerView.LayoutManager layoutManager = new LinearLayoutManager(
-                this);
-        Log.i("buju","34");
-        RecyclerView videoRecyclerView = findViewById(R.id.rv_video);
-        Log.i("find","38");
+        adapter = new VideoAdapter(this,videos);
+        //设置为list
+        //layoutManager = new LinearLayoutManager(this);
+      // 设置为grid显示
+        RecyclerView.LayoutManager layoutManager = new StaggeredGridLayoutManager(
+                2, StaggeredGridLayoutManager.VERTICAL
+        );
+        videoRecyclerView = findViewById(R.id.rv_video);
         videoRecyclerView.setAdapter(adapter);
-        Log.i("setAdapter","40");
         videoRecyclerView.setLayoutManager(layoutManager);
-        Log.i("Main","42");
-       // videoRecyclerView.addItemDecoration(new FolderItemDecoration(2));
-        Log.i("Main","44");
+        videoRecyclerView.addItemDecoration(new FolderItemDecoration(2));
     }
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.layout_change, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id = item.getItemId();
+        if (id == R.id.action_layout_list) {
+            layoutManager = new LinearLayoutManager(
+                    this);
+            adapter.notifyDataSetChanged();
+
+            return true;
+        } else if (id == R.id.action_layout_grid) {
+            layoutManager = new StaggeredGridLayoutManager(
+                    2, StaggeredGridLayoutManager.VERTICAL
+            );
+            adapter.notifyDataSetChanged();
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
 
 }
--- app/src/main/java/com/fxc/myvideoplayer/VideoList/VideoAdapter.java
+++ app/src/main/java/com/fxc/myvideoplayer/VideoList/VideoAdapter.java
@@ -22,16 +22,17 @@ public class VideoAdapter extends RecyclerView.Adapter<VideoAdapter.ViewHolder> 
     public VideoAdapter(Context context, List<VideoItems> videos) {
         this.context = context;
         this.videos = videos;
-        Log.i("constructor","21");
     }
 
     @Override
     public ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
-        Log.i("view holder on create","28");
         LayoutInflater inflater = LayoutInflater.from(parent.getContext());
-        View itemView = inflater.inflate(R.layout.file_item, parent, false);
-        return new ViewHolder(itemView);
+        //list显示
 
+        //View itemView = inflater.inflate(R.layout.file_item, parent, false);
+        //Grid 显示
+        View itemView = inflater.inflate(R.layout.video_item_grid, parent, false);
+        return new ViewHolder(itemView);
     }
 
     @Override
@@ -42,8 +43,6 @@ public class VideoAdapter extends RecyclerView.Adapter<VideoAdapter.ViewHolder> 
             .into(holder.video_image);
     holder.video_name.setText(videoItem.get_video_name());
     holder.video_duration.setText(videoItem.get_video_duration());
-
-
     }
 
     @Override
@@ -63,86 +62,8 @@ public class VideoAdapter extends RecyclerView.Adapter<VideoAdapter.ViewHolder> 
             video_duration= itemView.findViewById(R.id.video_duration);
             video_name = itemView.findViewById(R.id.video_name);
             video_image = itemView.findViewById(R.id.video_image);
-
         }
     }
-
 }
 
-
-
-/*
-import android.content.Context;
-import android.support.constraint.ConstraintLayout;
-import android.support.v7.widget.RecyclerView;
-import android.util.Log;
-import android.view.LayoutInflater;
-import android.view.View;
-import android.view.ViewGroup;
-import android.widget.ImageView;
-import android.widget.TextView;
-
-import com.bumptech.glide.Glide;
-
-import java.util.List;
-
-public class VideoAdapter extends RecyclerView.Adapter<VideoAdapter.ViewHolder1> {
-    Context context;
-    List<VideoItems> videos;
-
-    public VideoAdapter(Context context, List<VideoItems> videos) {
-        this.context = context;
-        this.videos = videos;
-        Log.i("onCreate","26");
-    }
-    @Override
-    public ViewHolder1 onCreateViewHolder(ViewGroup parent, int viewType) {
-        Log.i("onCreate","27");
-        LayoutInflater inflater = LayoutInflater.from(parent.getContext());
-        View itemView = inflater.inflate(R.layout.file_item, parent, false);
-        Log.i("onCreate","28");
-        return new ViewHolder1(itemView);
-
-    }
-
-    @Override
-    public void onBindViewHolder(ViewHolder1 holder, int position) {
-        final VideoItems video = videos.get(position);
-        Glide.with(context)
-                .load(video.get_image_Resource())
-                .into(holder.video_image);
-        holder.video_name.setText(video.get_video_name());
-        holder.video_duration.setText(video.get_video_duration());
-        holder.file_item.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                //TODO
-                //启动播放activity
-            }
-        });
-    }
-
-    @Override
-    public int getItemCount() {
-        Log.i("getItemCount","53");
-        return videos.size();
-    }
-
-    class ViewHolder1 extends RecyclerView.ViewHolder {
-        ConstraintLayout file_item;
-        TextView video_name;
-        TextView video_duration;
-        ImageView video_image;
-
-        public ViewHolder1(View itemView) {
-            super(itemView);
-            file_item = itemView.findViewById(R.id.file_item);
-            video_name = itemView.findViewById(R.id.video_name);
-            video_duration = itemView.findViewById(R.id.video_duration);
-            Log.i("view holder","66");
-            video_image = itemView.findViewById(R.id.video_image);
-
-        }
-    }
-    }*/
 
